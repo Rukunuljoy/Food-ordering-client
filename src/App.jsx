11e856379d79240cc5dd319be85f4d8b5b6a6c43
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-04-17 15:58:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className=" text-blue-50">Food ordering project</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Main from "../layout/Main";
+import Home from "../pages/home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children:[
+        {
+            path:'/',
+            element:<Home/>
+        }
+    ]
+  },
+]);
+
+export default router;
