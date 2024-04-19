21ea456755aa26fb6cfc5ffec52c6922c4b74766
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-04-19 16:15:52 +0600

    card add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../../components/Banner';
+import Categories from './Categories';
+import SpecialDeshes from './SpecialDeshes';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Categories/>
+            <SpecialDeshes/>
         </div>
     );
 };
