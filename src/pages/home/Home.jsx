3f20f25f8b0621d2065262203a51c95c26efdbba
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-04-20 15:48:30 +0600

    testimonial is completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../components/Banner';
 import Categories from './Categories';
 import SpecialDeshes from './SpecialDeshes';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <Categories/>
             <SpecialDeshes/>
+            <Testimonials/>
         </div>
     );
 };
