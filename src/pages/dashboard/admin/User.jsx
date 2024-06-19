4d04485caac163a214cbb515a5d8e4ba5819616b
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-06-19 21:24:27 +0600

    make admin and user delete action is completed

--- src/pages/dashboard/admin/User.jsx
+++ src/pages/dashboard/admin/User.jsx
@@ -1,16 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const User = () => {
+  const axiosSecure = useAxiosSecure();
     const { refetch, data: users = [] } = useQuery({
         queryKey: ["users"],
         queryFn: async () => {
-          const res = await fetch(`http://localhost:5000/users`);
-          return res.json();
+          const res = await axiosSecure.get('/users');
+          return res.data;
         },
       });
-  console.log({ users });
+  // console.log({ users });
+  const handleMakeAdmin = user =>{
+    axiosSecure.patch(`/users/admin/${user._id}`).then(res => {
+      alert(`${user.name} is now admin`);
+    })
+    refetch();
+  }
+
+  const handleDeleteUser = (user) =>{
+    axiosSecure.delete(`/users/${user._id}`).then(res => {
+      alert(`${user.name} is now deleted from database`);
+      refetch();
+    })
+  }
+
   return (
     <div>
      <div className="flex items-center justify-between m-4">
@@ -42,6 +58,7 @@ const User = () => {
                       "Admin"
                     ) : (
                       <button
+                      onClick={()=>handleMakeAdmin(user)}
                         className="btn btn-xs btn-circle bg-indigo-500 text-white"
                       >
                         <FaUsers />
@@ -49,7 +66,7 @@ const User = () => {
                     )}
                 </td>
                 <td>
-                  <button className="btn btn-xs bg-orange-500 text-white">
+                  <button onClick={()=> handleDeleteUser(user)} className="btn btn-xs bg-orange-500 text-white">
                     <FaTrashAlt />
                   </button>
                 </td>
