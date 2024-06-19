b5e6c5305f3191797ca40f5c325b85c3157b5dba
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-06-19 19:59:35 +0600

    axiosSecure like all code is secure

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("Access_token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // Add a response interceptor
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
