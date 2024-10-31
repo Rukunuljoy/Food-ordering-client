3c44ea83151e0982459a15f1271714c5cf9cee4a
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-10-31 16:41:28 +0600

    navbar issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ const Navbar = () => {
         </div>
 
      {/* Desktop Menu */}
-<div className="navbar-center hidden lg:flex">
+<div className="navbar-center lg:visible lg:flex">
   <ul className="menu menu-horizontal px-1">{navItems}</ul>
 </div>
 
